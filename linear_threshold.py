2eb88802fe2f8988d94e883ec6644c63927c36d4
marianaw <mariana.09.w@gmail.com>
2015-07-22 20:04:50 -0300

    Linear threshold model.

---
+++ linear_threshold.py
@@ -0,0 +1,70 @@
+from igraph import Graph
+from random import random
+
+
+def init_weights(g, w):
+    for e in g.es:
+        e.update_attributes({'weight': w})
+
+
+
+def init_node_probs(g):
+    for v in g.vs:
+        v.update_attributes({'prob': random()})
+    
+    
+
+def in_neighbours(g, v):
+    '''
+    Computes the in neighbours for the vertex v.
+    
+    Parameters:
+        @param g: the graph.
+        @param v: the vertex id.
+    
+    Returns:
+        A list of tuples with edges from the in neighbours.
+    '''
+    in_nbs = set()
+    for e in g.get_edgelist():
+        if e[1] == v:
+            in_nbs.add(e)
+    return in_nbs
+
+
+
+def one_step(g, v, seed):
+    in_nbs = in_neighbours(g, v)
+    active_nbs = [(n, v) for (n, v) in in_nbs if n in seed]
+    ss = 0
+    import ipdb; ipdb.set_trace()
+    vertex = g.vs.select(v)[0]
+    for n in active_nbs:
+        neigh = g.es.select(_source=n[0], _target=n[1])[0]
+        ss = ss + neigh.attributes()['weight']
+    if ss >= vertex.attributes()['prob']:
+        seed = seed + [v]
+    return seed
+
+
+    
+def linear_threshold(g, seed):
+    init_weights(g, 0.95)
+    init_node_probs(g)
+    B = [seed]
+    vs = set([i for (i, _) in g.get_edgelist()] + [i for (_, i) in g.get_edgelist()])
+    for v in vs:
+        if v not in seed:
+            seed = one_step(g, v, seed)
+        B = B + [seed]
+    return seed, B    
+            
+            
+            
+if __name__ == '__main__':
+    graph = Graph.Read_Edgelist(open('g.txt', 'r'))
+    seed = [24394]
+    A, B = linear_threshold(graph, seed)
+    import ipdb; ipdb.set_trace()
+    for i in B:
+        print(i)
